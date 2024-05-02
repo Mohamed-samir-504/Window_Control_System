de593f4d0586e77fe201755c5a4c5516869f5a7e
markkashraf <mark2001rko2001@gmail.com>
2024-05-02 13:31:33 +0300

    test

--- Drivers/UART.h
+++
@@ -1,5 +0,0 @@
-void Delay(unsigned long counter);
-char UART0_Receiver(void);
-void UART0_Transmitter(unsigned char data);
-void printstring(char *str);
-void UART_Init();
--- Objects/main.d
+++
@@ -1,2 +0,0 @@
-./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h Drivers\UART.h
