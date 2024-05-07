9e7bbdd33b712fbd9fc3f7c59f0279e3885048f1
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2024-05-07 18:17:28 +0300

    tasks prototypes and interrupt handler

--- main.c
+++ main.c
@@ -9,6 +9,32 @@
 #include "queue.h"
 #include "semphr.h"
 
+//will be unblocked after jam interrupt
+void Jam(void* pvParameters){
+
+}
+
+//will be unblocked after lock interrupt
+void Lock(void* pvParameters){
+
+}
+
+//will poll on driver buttons
+void Driver(void* pvParameters){
+
+}
+
+//will poll on passenger buttons
+void Passenger(void* pvParameters){
+
+}
+
+//will check for the queue shared between functions and take action based on value
+void MotorAction(void* pvParameters){
+
+}
+
+
 
 
 int main( void )
@@ -18,4 +44,20 @@ int main( void )
 	INIT_LOCK_SW();
 	INIT_JAM_BUTTON();
 	while(1);
+}
+
+
+void GPIOE_Handler(){
+
+	// if jam button caused the interrupt
+	if (READ_JAM_BUTTON()){
+		//TO DO
+		//give jam semaphore to unblock jam task
+	}
+
+	//if lock button caused the interrupt
+	else if (READ_LOCK_SW()){
+		//TO DO
+		//give lock semaphore to unblock lock task
+	}
 }
