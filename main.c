f100e6e48b4890a8a0ca7332d44f4cecf5e58e70
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2024-05-11 11:59:24 +0300

    created passenger task

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ SemaphoreHandle_t xJamSemaphore = NULL;
 SemaphoreHandle_t xLockSemaphore = NULL;
 SemaphoreHandle_t xPassengerWindowMutex = NULL;
 xQueueHandle xMotorCommandQueue; 
-
+TaskHandle_t xDriverHandle;
 
 //will be unblocked after jam interrupt
 static void vJamTask(void* pvParameters){
@@ -24,11 +24,18 @@ static void vJamTask(void* pvParameters){
 
 //will be unblocked after lock interrupt
 void vLockTask(void* pvParameters){
-	xSemaphoreTake(xLockSemaphore,portMAX_DELAY);
 
-	//TO DO
-	//Raise driver task priority if lock is on
-	//Return driver task priority to normal if lock is off
+	for(;;){
+		xSemaphoreTake(xLockSemaphore,portMAX_DELAY);
+
+		if(READ_LOCK_SW() == 1){
+			
+			GPIOF->DATA
+		}
+	}
+	
+
+	
 
 }
 
@@ -87,7 +94,44 @@ void vPassengerTask(void* pvParameters){
 	
 for(;;){
 	xSemaphoreTake(xPassengerWindowMutex, portMAX_DELAY);
-	
+	if(READ_PASSENGER_OPEN_BUTTON() && READ_PASSENGER_CLOSE_BUTTON()) // both buttons shouldn't be pressed together.
+	{
+		xSemaphoreGive(xPassengerWindowMutex);
+		continue; 
+	}
+
+	uint8_t button_open_before  =  READ_PASSENGER_OPEN_BUTTON();
+	uint8_t button_close_before =  READ_PASSENGER_CLOSE_BUTTON();
+
+	vTaskDelay(50/portTICK_RATE_MS); // sample and wait to see if the user is still holding the button
+
+	if(!READ_PASSENGER_CLOSE_BUTTON() && button_close_before && !READ_LIMIT_SW1()) // if the passenger has pressed the close button without holding.
+	{
+		vSendMotorCommandToBack(&xMotorCommandQueue,UP);
+		while (!READ_LIMIT_SW1());   // while the limit is not reached, wait.
+		vSendMotorCommandToBack(&xMotorCommandQueue,OFF);
+	}
+
+	else if (READ_PASSENGER_CLOSE_BUTTON() && button_close_before && !READ_LIMIT_SW1()) // if the passenger is holding the close button.
+	{
+		vSendMotorCommandToBack(&xMotorCommandQueue,UP);
+		while (READ_PASSENGER_CLOSE_BUTTON() && !READ_LIMIT_SW1()); // while the passenger is pressing the close button and the limit is not reached.
+		vSendMotorCommandToBack(&xMotorCommandQueue,OFF);
+	}
+
+	if(!READ_PASSENGER_OPEN_BUTTON() && button_open_before&& !READ_LIMIT_SW2()) // if the passenger has pressed the open button without holding.
+	{	
+		vSendMotorCommandToBack(&xMotorCommandQueue,DOWN);
+		while (!READ_LIMIT_SW2()); // while the limit is not reached, wait.
+		vSendMotorCommandToBack(&xMotorCommandQueue,OFF);
+	}
+
+	else if (READ_PASSENGER_OPEN_BUTTON() && button_open_before&& !READ_LIMIT_SW2()) // if the passenger is holding the open button.
+	{
+		vSendMotorCommandToBack(&xMotorCommandQueue,DOWN);
+		while (READ_PASSENGER_OPEN_BUTTON() && !READ_LIMIT_SW2()); // while the passenger is pressing the open button and the limit is not reached.
+		vSendMotorCommandToBack(&xMotorCommandQueue,OFF);
+	}
 	
 	xSemaphoreGive(xPassengerWindowMutex);
 	}
@@ -129,9 +173,11 @@ int main( void )
 	xLockSemaphore = xSemaphoreCreateBinary();
 	xPassengerWindowMutex = xSemaphoreCreateMutex();
 
-	xTaskCreate(vJamTask, "Jam Task", 64, NULL, 2, NULL);
-	xTaskCreate(vDriverTask, "Driver Task", 64, NULL, 2, NULL);
-	xTaskCreate(vMotorAction, "Motor Task", 64, NULL, 3, NULL);
+	xTaskCreate(vDriverTask, "Driver Task", 64, NULL, 1, NULL);
+	xTaskCreate(vPassengerTask, "Passenger Task", 64, NULL, 1, NULL);
+	xTaskCreate(vJamTask, "Jam Task", 64, NULL, 3, NULL);
+	xTaskCreate(vLockTask, "Lock Task", 64, NULL, 3, NULL);
+	xTaskCreate(vMotorAction, "Motor Task", 64, NULL, 4, NULL);
 
 	vTaskStartScheduler();
 
@@ -146,13 +192,14 @@ void GPIOE_Handler(){
 
 	// if jam button caused the interrupt
 	if (GPIOE->RIS & 0x4) {
-	vPrintString("Semaphore given");
+	vPrintString("Jam Semaphore given");
 	xSemaphoreGiveFromISR(xJamSemaphore,NULL);
 	}
 	
 
 	//if lock button caused the interrupt
 	else if (GPIOE->RIS & 0x2){
+		vPrintString("Lock Semaphore given");
 		xSemaphoreGiveFromISR(xLockSemaphore,NULL);
 	}
 
